8b53ea53f2a9b8f574496aa73d7ac29e35d498b7
root <root@git.europe-west2-c.c.proud-research-234114.internal>
2019-04-30 02:57:27 +0000

    Added comment live in basic code and created a new file

--- Basic.java
+++ Basic.java
@@ -4,6 +4,7 @@ Public class Basic
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
+        //Print welcome Message
         System.out.println("Welcome to jungle"); 
     } 
 } 
---
+++ WRITEME.md
@@ -0,0 +1,8 @@
+This is GIT initialisation
+Creating Two files and will commit later
+
+Team Members
+Jatin Bisht
+Nitin Bisht
+Hitesh kumar
+Gaurav singh
